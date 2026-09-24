11f540708c605468c9e4f509d5795bc5592850b9
trung150704 <trungnbps31077@fpt.edu.vn>
2026-09-24 23:15:19 +0700

    Buoi02 Bai02 gioi thieu ban than

---
+++ Bai02_GioiThieuBanThan.kt.kts
@@ -0,0 +1,10 @@
+//Buoi02Bai01 gioi thieu ban than, Nguyen Bao Trung - 25810047
+fun gioiThieu(){
+    val hoTen = "Nguyen Bao Trung"
+    val namSinh = 2004
+    val namHienTai = 2026
+    val tuoi = namHienTai - namSinh
+
+    println("Toi la: $hoTen , nam nay: $tuoi tuoi")
+}
+gioiThieu()
