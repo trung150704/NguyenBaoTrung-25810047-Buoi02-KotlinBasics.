f935514ce7c1a79913cc3675557b134ddd7f3600
trung150704 <trungnbps31077@fpt.edu.vn>
2026-09-26 15:18:24 +0700

    Bai02_TinhDienTich.kts

---
+++ D3/Bai02_TinhDienTich.kts
@@ -0,0 +1,10 @@
+//Nguyen Bao Trung - 25810047
+fun tinhDienTich(chieuDai: Double, chieuRong: Double): Double {
+    return chieuDai * chieuRong
+}
+
+val ketQua1 = tinhDienTich(5.5, 3.0)
+println("Dien tich hinh chu nhat 1 (5.5 x 3.0) là: $ketQua1")
+
+val ketQua2 = tinhDienTich(12.0, 4.2)
+println("Dien tich hinh chu nhat 2 (12.0 x 4.2) là: $ketQua2")
