5eb550b21578b898969531e14cd96e2c228a3c1f
trung150704 <trungnbps31077@fpt.edu.vn>
2026-09-26 15:18:34 +0700

    Bai03_GhiNhatKy.kts

---
+++ D3/Bai03_GhiNhatKy.kts
@@ -0,0 +1,12 @@
+//Nguyen Bao Trung - 25810047
+fun ghiNhatKyTuongMinh(hanhDong: String): Unit {
+    println("[LOG]: $hanhDong")
+}
+fun ghiNhatKyRutGon(hanhDong: String) {
+    println("[LOG]: $hanhDong")
+}
+fun main() {
+    ghiNhatKyTuongMinh("Người dùng đăng nhập vào hệ thống")
+    ghiNhatKyRutGon("Người dùng đăng nhập vào hệ thống")
+}
+main()
